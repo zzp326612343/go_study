be2aa0b8e537e1bbeb30a44cd2908e12f6fe17e4
zhipeng <326612342@qq.com>
2025-07-18 17:58:52 +0700

    dapp练习

--- go.mod
+++ go.mod
@@ -3,20 +3,37 @@ module github.com/zzp326612343/go_study
 go 1.24.4
 
 require (
+	github.com/ethereum/go-ethereum v1.16.1
+	github.com/golang-jwt/jwt/v4 v4.5.2
+)
+
+require (
 	filippo.io/edwards25519 v1.1.0 // indirect
+	github.com/Microsoft/go-winio v0.6.2 // indirect
+	github.com/StackExchange/wmi v1.2.1 // indirect
+	github.com/bits-and-blooms/bitset v1.20.0 // indirect
 	github.com/bytedance/sonic v1.13.3 // indirect
 	github.com/bytedance/sonic/loader v0.3.0 // indirect
 	github.com/cloudwego/base64x v0.1.5 // indirect
 	github.com/cloudwego/iasm v0.2.0 // indirect
+	github.com/consensys/gnark-crypto v0.18.0 // indirect
+	github.com/crate-crypto/go-eth-kzg v1.3.0 // indirect
+	github.com/crate-crypto/go-ipa v0.0.0-20240724233137-53bbb0ceb27a // indirect
+	github.com/deckarep/golang-set/v2 v2.6.0 // indirect
+	github.com/decred/dcrd/dcrec/secp256k1/v4 v4.0.1 // indirect
+	github.com/ethereum/c-kzg-4844/v2 v2.1.0 // indirect
+	github.com/ethereum/go-verkle v0.2.2 // indirect
 	github.com/gabriel-vasile/mimetype v1.4.9 // indirect
 	github.com/gin-contrib/sse v1.1.0 // indirect
 	github.com/gin-gonic/gin v1.10.1 // indirect
+	github.com/go-ole/go-ole v1.3.0 // indirect
 	github.com/go-playground/locales v0.14.1 // indirect
 	github.com/go-playground/universal-translator v0.18.1 // indirect
 	github.com/go-playground/validator/v10 v10.27.0 // indirect
 	github.com/go-sql-driver/mysql v1.9.3 // indirect
 	github.com/goccy/go-json v0.10.5 // indirect
-	github.com/golang-jwt/jwt/v4 v4.5.2
+	github.com/gorilla/websocket v1.4.2 // indirect
+	github.com/holiman/uint256 v1.3.2 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.5 // indirect
 	github.com/jmoiron/sqlx v1.4.0 // indirect
@@ -28,11 +45,16 @@ require (
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.2 // indirect
 	github.com/pelletier/go-toml/v2 v2.2.4 // indirect
+	github.com/shirou/gopsutil v3.21.4-0.20210419000835-c7a38de76ee5+incompatible // indirect
+	github.com/supranational/blst v0.3.14 // indirect
+	github.com/tklauser/go-sysconf v0.3.12 // indirect
+	github.com/tklauser/numcpus v0.6.1 // indirect
 	github.com/twitchyliquid64/golang-asm v0.15.1 // indirect
 	github.com/ugorji/go/codec v1.3.0 // indirect
 	golang.org/x/arch v0.19.0 // indirect
 	golang.org/x/crypto v0.39.0 // indirect
 	golang.org/x/net v0.41.0 // indirect
+	golang.org/x/sync v0.16.0 // indirect
 	golang.org/x/sys v0.34.0 // indirect
 	golang.org/x/text v0.27.0 // indirect
 	google.golang.org/protobuf v1.36.6 // indirect
---
+++ go_lianxi1/main.go
@@ -0,0 +1,199 @@
+package main
+
+import (
+	"context"
+	"crypto/ecdsa"
+	"fmt"
+	"log"
+	"math"
+	"math/big"
+	"reflect"
+
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/common/hexutil"
+	"github.com/ethereum/go-ethereum/core/types"
+	"github.com/ethereum/go-ethereum/crypto"
+	"github.com/ethereum/go-ethereum/ethclient"
+	"github.com/ethereum/go-ethereum/rpc"
+	"golang.org/x/crypto/sha3"
+)
+
+func main() {
+	client, err := ethclient.Dial("https://eth-sepolia.g.alchemy.com/v2/3uyYJlPtrNVbiZnhihQCE3rDbbXYJaUV")
+	if err != nil {
+		log.Fatal(err)
+	}
+	queryBlockBalance(client)
+}
+
+func queryBlock(client *ethclient.Client) {
+
+	// blockNumber := big.NewInt(5671744)
+	header, err := client.HeaderByNumber(context.Background(), nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(header.Number.Uint64())
+	fmt.Println(header.Time)
+	fmt.Println(header.Difficulty.Uint64())
+	fmt.Println(header.Hash().Hex())
+
+	block, err := client.BlockByNumber(context.Background(), nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(block.Number().Uint64())
+	fmt.Println(block.Time())
+	fmt.Println(block.Difficulty().Uint64())
+	fmt.Println(block.Hash().Hex())
+	fmt.Println(len(block.Transactions()))
+	count, err := client.TransactionCount(context.Background(), block.Hash())
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(count)
+}
+
+func queryTransaction(client *ethclient.Client) {
+	chainId, err := client.ChainID(context.Background())
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(chainId)
+	blockNumber := big.NewInt(5671744)
+	block, err := client.BlockByNumber(context.Background(), blockNumber)
+	if err != nil {
+		log.Fatal(err)
+	}
+	trasanctions := block.Transactions()
+	for _, tx := range trasanctions {
+		fmt.Println(tx.Hash().Hex())
+		fmt.Println(tx.Value().String())
+		fmt.Println(tx.Gas())
+		fmt.Println(tx.GasPrice().String())
+		fmt.Println(tx.Nonce())
+		fmt.Println(tx.To().Hex())
+		if sender, err := types.Sender(types.NewEIP155Signer(chainId), tx); err == nil {
+			fmt.Println("sender", sender.Hex())
+		} else {
+			log.Fatal(err)
+		}
+		receipt, err := client.TransactionReceipt(context.Background(), tx.Hash())
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Println("receipt", receipt.Status)
+		fmt.Println(receipt.Logs)
+		break
+	}
+	blockHash := common.HexToHash("0xae713dea1419ac72b928ebe6ba9915cd4fc1ef125a606f90f5e783c47cb1a4b5")
+	count, err := client.TransactionCount(context.Background(), blockHash)
+	if err != nil {
+		log.Fatal(err)
+	}
+	for i := uint(0); i < count; i++ {
+		tx, err := client.TransactionInBlock(context.Background(), blockHash, i)
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Println("blockInHash", tx.Hash().Hex())
+		break
+	}
+	txHash := common.HexToHash("0x20294a03e8766e9aeab58327fc4112756017c6c28f6f99c7722f4a29075601c5")
+	tx, isPending, err := client.TransactionByHash(context.Background(), txHash)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(isPending)
+	fmt.Println(tx.Hash().Hex()) // 0x20294a03e8766e9aeab58327fc4112756017c6c28f6f99c7722f4a29075601c5.Println(isPending)       // false
+}
+
+func queryReceipt(client *ethclient.Client) {
+	blockNumber := big.NewInt(5671744)
+	blockHash := common.HexToHash("0xae713dea1419ac72b928ebe6ba9915cd4fc1ef125a606f90f5e783c47cb1a4b5")
+	receipByHash, err := client.BlockReceipts(context.Background(), rpc.BlockNumberOrHashWithHash(blockHash, false))
+	if err != nil {
+		log.Fatal(err)
+	}
+	receipByBlockNumber, err := client.BlockReceipts(context.Background(), rpc.BlockNumberOrHashWithNumber(rpc.BlockNumber(blockNumber.Int64())))
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(reflect.DeepEqual(receipByHash[0], receipByBlockNumber[0]))
+}
+
+func cryptoKey(client *ethclient.Client) {
+	privateKey, err := crypto.GenerateKey()
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(privateKey)
+	privateKeyBytes := crypto.FromECDSA(privateKey)
+	fmt.Println(hexutil.Encode(privateKeyBytes[2:]))
+	publicKey := privateKey.Public()
+	publicKeyECDSA, ok := publicKey.(*ecdsa.PublicKey)
+	if !ok {
+		log.Fatal("cannot assert type: publicKey is not of type *ecdsa.PublicKey")
+	}
+	publicKeyBytes := crypto.FromECDSAPub(publicKeyECDSA)
+	fmt.Println(hexutil.Encode(publicKeyBytes[4:]))
+
+	address := crypto.PubkeyToAddress(*publicKeyECDSA).Hex()
+	fmt.Println(address)
+
+	hash := sha3.NewLegacyKeccak256()
+	hash.Write(publicKeyBytes[1:])
+	fmt.Println("full:", hexutil.Encode(hash.Sum(nil)[:]))
+	fmt.Println(hexutil.Encode(hash.Sum(nil)[12:]))
+}
+
+func ethAToB(client *ethclient.Client) {
+	privateKey, err := crypto.HexToECDSA("发送地址私钥")
+	if err != nil {
+		log.Fatal(err)
+	}
+	publicKey := privateKey.Public()
+	publicKeyECDSA, ok := publicKey.(*ecdsa.PublicKey)
+	if !ok {
+		log.Fatal("cannot assert type: publicKey is not of type *ecdsa.PublicKey")
+	}
+	fromAddress := crypto.PubkeyToAddress(*publicKeyECDSA)
+	nonce, err := client.PendingNonceAt(context.Background(), fromAddress)
+	if err != nil {
+		log.Fatal(err)
+	}
+	gasPrice, err := client.SuggestGasPrice(context.Background())
+	if err != nil {
+		log.Fatal(err)
+	}
+	gasLimit := uint64(21000)
+	value := big.NewInt(1000000000000000000)
+	toAddress := common.HexToAddress("接收地址")
+	tx := types.NewTransaction(nonce, toAddress, value, gasLimit, gasPrice, nil)
+	chainID, err := client.NetworkID(context.Background())
+	if err != nil {
+		log.Fatal(err)
+	}
+	signedTx, err := types.SignTx(tx, types.NewEIP155Signer(chainID), privateKey)
+	if err != nil {
+		log.Fatal(err)
+	}
+	err = client.SendTransaction(context.Background(), signedTx)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(signedTx.Hash().Hex())
+}
+
+func queryBlockBalance(client *ethclient.Client) {
+	address := common.HexToAddress("0xd19a554FeEa3d76F91bb98390fE37975a58337f1")
+	balance, err := client.BalanceAt(context.Background(), address, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(balance)
+	fbalance := new(big.Float)
+	fbalance.SetString(balance.String())
+	ebalance := new(big.Float).Quo(fbalance, big.NewFloat(math.Pow10(18)))
+	fmt.Println(ebalance)
+}
